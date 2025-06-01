4e8316dcfb4416ec4dd886e718364230b5a22460
MarcoCostantini26 <marco.costantini26@gmail.com>
2025-06-01 13:19:07 +0200

    implement ObjectiveValidator and ObjectiveCard

--- src/core/src/main/scala/engine/ObjectiveValidator.scala
+++ src/core/src/main/scala/engine/ObjectiveValidator.scala
@@ -1,2 +1,37 @@
 package engine
 
+import model.cards.*
+import model.player.*
+import model.board.*
+
+object ObjectiveValidator:
+
+  def isCompleted(objective: ObjectiveCard, gameState: GameState, playerState: PlayerState): Boolean =
+    objective match
+      case ObjectiveCard.ConquerTerritories(num, minTroops) =>
+        // Conta i territori posseduti con almeno minTroops truppe
+        val owned = gameState.board.territories.count: t =>
+          t.owner.exists(_.id == playerState.playerId) && t.troops >= minTroops
+        
+        owned >= num
+
+      case ObjectiveCard.ConquerContinents(continents) =>
+        // Verifica se il giocatore possiede tutti i territori di ciascun continente richiesto
+        continents.forall: continent =>
+          continent.territories.forall: t =>
+            gameState.board.territories.find(_.name == t.name).exists(_.owner.exists(_.id == playerState.playerId))
+        
+
+      case ObjectiveCard.EliminatePlayer(targetColor) =>
+        // Verifica se il giocatore target non possiede più territori
+        !gameState.board.territories.exists(_.owner.exists(_.color == targetColor))
+
+      case ObjectiveCard.ConquerNContinents(n) =>
+        // Conta i continenti completamente posseduti
+        val ownedContinents = gameState.board.continents.count: continent =>
+          continent.territories.forall: t =>
+            gameState.board.territories.find(_.name == t.name).exists(_.owner.exists(_.id == playerState.playerId))
+        ownedContinents >= n
+
+      case _ => false
+
--- src/core/src/main/scala/model/cards/ObjectiveCard.scala
+++ src/core/src/main/scala/model/cards/ObjectiveCard.scala
@@ -1,3 +1,19 @@
 package model.cards
 
-case class ObjectiveCard() extends Card
+import model.player.PlayerColor
+import model.board.Continent
+
+sealed trait ObjectiveCard extends Card
+
+object ObjectiveCard:
+  /** Conquista almeno `num` territori, ognuno con almeno `minTroops` truppe */
+  case class ConquerTerritories(num: Int, minTroops: Int = 1) extends ObjectiveCard
+
+  /** Conquista tutti i territori dei continenti specificati */
+  case class ConquerContinents(continents: Set[Continent]) extends ObjectiveCard
+
+  /** Elimina il giocatore di un certo colore */
+  case class EliminatePlayer(targetColor: PlayerColor) extends ObjectiveCard
+
+  /** Conquista almeno `n` continenti a scelta */
+  case class ConquerNContinents(n: Int) extends ObjectiveCard
