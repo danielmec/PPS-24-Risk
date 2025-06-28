d8d4b1b407c6860d20bf4772b0486cd6379128f3
MarcoCostantini26 <marco.costantini26@gmail.com>
2025-06-28 17:22:03 +0200

    merging the changes

--- src/core/src/main/scala/engine/TurnManager.scala
+++ src/core/src/main/scala/engine/TurnManager.scala
@@ -27,64 +27,66 @@ case class TurnManagerImpl(
             // if nextIndex grows beyond the list size, it wraps around
             val nextIndex = (currentPlayerIndex + 1) % players.size
             if (phase == TurnPhase.SetupPhase && nextIndex != 0)
-                // Durante il setup, rimaniamo in SetupPhase
-                println("HEY HEY SONO PROPRIO QUI")
                 copy(currentPlayerIndex = nextIndex, phase = TurnPhase.SetupPhase)
             else
-                // Dopo il setup o quando il turno torna al primo giocatore, passiamo alla MainPhase
-                println("HEY HEY MI SONO PROPRIO SPOSTATO")
                 copy(currentPlayerIndex = nextIndex, phase = TurnPhase.MainPhase)
 
     def currentPhase: TurnPhase = phase
 
     def isValidAction(action: GameAction, gameState: GameState, engineState: EngineState): Boolean = (action, phase) match
-        // Piazzamento truppe consentito in entrambe le fasi
+
         case (GameAction.PlaceTroops(playerId, troops, territoryName), _) => 
             val playerState = gameState.getPlayerState(playerId).getOrElse(throw new InvalidActionException())
-            val territory = gameState.getTerritoryByName(territoryName).getOrElse(throw new InvalidActionException())
+            val territory = gameState.getTerritoryByName(territoryName).getOrElse(throw new InvalidActionException())               
+            val phaseValidation = phase match
+                case TurnPhase.SetupPhase => true  
+                case TurnPhase.MainPhase => playerState.bonusTroops >= troops  
             playerId == currentPlayer.id && 
             troops > 0 &&
-            territory.isOwnedBy(playerId) &&
-            (phase == TurnPhase.SetupPhase || playerState.bonusTroops >= troops)
-            
-        // Rinforzo consentito solo in MainPhase
+            territory.isOwnedBy(playerId) && 
+            phaseValidation
+        
+        case (GameAction.EndSetup, TurnPhase.SetupPhase) => 
+            val playerState = gameState.getPlayerState(currentPlayer.id).getOrElse(throw new InvalidActionException())
+            playerState.bonusTroops == 0
+        
         case (GameAction.Reinforce(playerId, from, to, numTroops), TurnPhase.MainPhase) =>
-          val playerState = gameState.getPlayerState(playerId).getOrElse(throw new InvalidActionException())
-          val fromTerritory = gameState.getTerritoryByName(from).getOrElse(throw new InvalidActionException())
-          val toTerritory = gameState.getTerritoryByName(to).getOrElse(throw new InvalidActionException())
-          playerId == currentPlayer.id &&
-          playerState.bonusTroops == 0 &&
-          fromTerritory.isOwnedBy(playerId) &&
-          toTerritory.isOwnedBy(playerId) &&
-          fromTerritory.hasEnoughTroops(numTroops + 1) &&
-          gameState.board.areNeighbors(fromTerritory, toTerritory)
+            val playerState = gameState.getPlayerState(playerId).getOrElse(throw new InvalidActionException())
+            val fromTerritory = gameState.getTerritoryByName(from).getOrElse(throw new InvalidActionException())
+            val toTerritory = gameState.getTerritoryByName(to).getOrElse(throw new InvalidActionException())
+            playerId == currentPlayer.id &&
+            playerState.bonusTroops == 0 &&  
+            fromTerritory.isOwnedBy(playerId) &&
+            toTerritory.isOwnedBy(playerId) &&
+            fromTerritory.hasEnoughTroops(numTroops + 1) &&  
+            gameState.board.areNeighbors(fromTerritory, toTerritory)
             
-        // Attacco consentito solo in MainPhase
         case (GameAction.Attack(attackerId, defenderId, from, to, numTroops), TurnPhase.MainPhase) =>
             val playerState = gameState.getPlayerState(attackerId).getOrElse(throw new InvalidActionException())
             val fromTerritory = gameState.getTerritoryByName(from).getOrElse(throw new InvalidActionException())
             val toTerritory = gameState.getTerritoryByName(to).getOrElse(throw new InvalidActionException())
             attackerId == currentPlayer.id && 
             attackerId != defenderId && 
-            playerState.bonusTroops == 0 && // Il giocatore deve aver piazzato tutte le truppe bonus
+            playerState.bonusTroops == 0 &&  
             numTroops > 0 &&
             fromTerritory.isOwnedBy(attackerId) && 
             toTerritory.isOwnedBy(defenderId) &&
-            fromTerritory.hasEnoughTroops(numTroops + 1) &&
+            fromTerritory.hasEnoughTroops(numTroops + 1) &&  
             gameState.board.areNeighbors(fromTerritory, toTerritory)
             
-        // Scambio carte consentito solo in MainPhase
         case (GameAction.TradeCards(territoryCards), TurnPhase.MainPhase) => 
             val playerState = gameState.getPlayerState(currentPlayer.id).getOrElse(throw new InvalidActionException())  
+            
             territoryCards.size == 3 && 
             territoryCards.subsetOf(playerState.territoryCards) &&
             BonusCalculator.calculateTradeBonus(territoryCards) > 0
         
-        // EndSetup è valido solo in SetupPhase
-        case (GameAction.EndSetup, TurnPhase.SetupPhase) => true
+        case (GameAction.EndTurn, TurnPhase.MainPhase) => 
+            val playerState = gameState.getPlayerState(currentPlayer.id).getOrElse(throw new InvalidActionException())
+            playerState.bonusTroops == 0  
         
-        // EndTurn sempre valido
-        case (GameAction.EndTurn, _) => true
+        case (GameAction.EndTurn, TurnPhase.SetupPhase) => 
+            val playerState = gameState.getPlayerState(currentPlayer.id).getOrElse(throw new InvalidActionException())
+            playerState.bonusTroops == 0  
         
-        // Altri casi non validi
         case _ => false
--- src/core/src/main/scala/utils/GameEngineUtils.scala
+++ src/core/src/main/scala/utils/GameEngineUtils.scala
@@ -4,6 +4,8 @@ import model.player.*
 import model.cards.*
 import model.board.*
 import engine.*
+import exceptions._
+import scala.util.Random
 
 object GameEngineUtils:
   
@@ -11,35 +13,24 @@ object GameEngineUtils:
     gameState: GameState, 
     eliminatedPlayerId: String,
     conquererPlayerId: String
-  ): GameState = {
-    val eliminatedPlayer = gameState.getPlayerState(eliminatedPlayerId).getOrElse(
-      throw new IllegalArgumentException(s"Player $eliminatedPlayerId not found")
-    )
-    val conquererPlayer = gameState.getPlayerState(conquererPlayerId).getOrElse(
-      throw new IllegalArgumentException(s"Player $conquererPlayerId not found")
-    )
+  ): GameState = 
+    val eliminatedPlayer = gameState.getPlayerState(eliminatedPlayerId).getOrElse(throw new InvalidPlayerException())
+    val conquererPlayer = gameState.getPlayerState(conquererPlayerId).getOrElse(throw new InvalidPlayerException())
     val eliminatedCards = eliminatedPlayer.territoryCards
-    val updatedConquerState = conquererPlayer.copy(
-      territoryCards = conquererPlayer.territoryCards ++ eliminatedCards
-    )
+    val updatedConquerState = conquererPlayer.addTerritoryCards(eliminatedCards)
     gameState
       .updatePlayerState(conquererPlayerId, updatedConquerState)
-      .updatePlayerState(eliminatedPlayerId, eliminatedPlayer.copy(territoryCards = Set.empty))
-  }
+      .updatePlayerState(eliminatedPlayerId, eliminatedPlayer.removeTerritoryCards(eliminatedPlayer.territoryCards))
 
   def drawTerritoryCard(
     gameState: GameState,
     decksManager: DecksManager,
     playerId: String
-  ): (GameState, DecksManager) = {
+  ): (GameState, DecksManager) = 
     try {
       val (updatedDecksManager, card) = decksManager.drawTerritory()
-      val playerState = gameState.getPlayerState(playerId).getOrElse(
-        throw new IllegalArgumentException(s"Player $playerId not found")
-      )
-      val updatedPlayerState = playerState.copy(
-        territoryCards = playerState.territoryCards + card
-      )     
+      val playerState = gameState.getPlayerState(playerId).getOrElse(throw new InvalidPlayerException())
+      val updatedPlayerState = playerState.addTerritoryCard(card)     
       val updatedGameState = gameState
         .updatePlayerState(playerId, updatedPlayerState)
         .updateDecksManager(updatedDecksManager)    
@@ -47,16 +38,57 @@ object GameEngineUtils:
     } catch {
       case e: Exception => (gameState, decksManager)
     }
-  }
 
-  def hasRemainingTerritories(gameState: GameState, playerId: String): Boolean =
-    gameState.board.territories.exists(_.owner.exists(_.id == playerId))
+  def hasRemainingTerritories(gameState: GameState, playerId: String): Boolean = gameState.board.territoriesOwnedBy(playerId).nonEmpty
 
-  def countPlayerTerritories(gameState: GameState, playerId: String): Int =
-    gameState.board.territories.count(_.owner.exists(_.id == playerId))
+  def countPlayerTerritories(gameState: GameState, playerId: String): Int = gameState.board.territoriesOwnedBy(playerId).size
 
   def getAdjacentTerritories(gameState: GameState, territoryName: String, playerId: String): Set[Territory] =
-    val territory = gameState.getTerritoryByName(territoryName).getOrElse(
-      throw new IllegalArgumentException(s"Territory $territoryName not found")
+    val territory = gameState.getTerritoryByName(territoryName).getOrElse(throw new InvalidTerritoryException())
+    territory.neighbors.filter(_.isOwnedBy(playerId)).toSet 
+
+  def distributeInitialTerritories(board: Board, players: List[PlayerImpl]): Board =
+    val shuffledTerritories = Random.shuffle(board.territories)
+    val assignedTerritories = shuffledTerritories.zipWithIndex.map: 
+      case (territory, index) =>
+        val playerIndex = index % players.size
+        val player = players(playerIndex)
+        territory.changeOwner(player).addTroops(1)
+    assignedTerritories.foldLeft(board):
+      (updatedBoard, territory) => updatedBoard.updatedTerritory(territory)
+  
+  def assignObjectivesToPlayers(currentDecksManager: DecksManager, players: List[PlayerImpl], playerStates: List[PlayerState]): (DecksManager, List[PlayerState]) = 
+    players.foldLeft((currentDecksManager, List.empty[PlayerState])):
+      case ((dm, states), player) =>
+        val (updatedDM, objective) = dm.drawObjective()
+        val playerState = playerStates.find(_.playerId == player.id).get
+        (updatedDM, states :+ playerState.setObjectiveCard(objective))
+  
+  def handleTerritoryCardReward(state: EngineState, action: GameAction): EngineState =
+    val gameState = state.gameState
+    if (action == GameAction.EndTurn && state.territoryConqueredThisTurn)
+      val currentPlayerId = gameState.turnManager.currentPlayer.id
+      val (updatedGameState, _) = drawTerritoryCard(gameState, gameState.decksManager, currentPlayerId)
+      state.copy(gameState = updatedGameState, territoryConqueredThisTurn = false)
+    else state
+
+  def moveToNextPlayer(state: EngineState): EngineState =
+    val gameState = state.gameState
+    val nextTurnManager = gameState.turnManager.nextPlayer()
+    var updatedGameState = gameState.updateTurnManager(nextTurnManager)
+    if (nextTurnManager.currentPhase == TurnPhase.MainPhase) 
+      val nextPlayerId = nextTurnManager.currentPlayer.id
+      val nextPlayerState = updatedGameState.getPlayerState(nextPlayerId).get
+      val bonus = BonusCalculator.calculateStartTurnBonus(nextPlayerId, updatedGameState.board)
+      val updatedPlayerState = nextPlayerState.copy(bonusTroops = bonus)
+      updatedGameState = updatedGameState.updatePlayerState(nextPlayerId, updatedPlayerState)
+    state.copy(
+      gameState = updatedGameState,
+      territoryConqueredThisTurn = state.territoryConqueredThisTurn
     )
-    territory.neighbors.filter(_.isOwnedBy(playerId)).toSet
+
+  def checkVictoryCondition(state: EngineState): EngineState =
+    val optPlayerState = state.gameState.checkWinCondition
+    optPlayerState match
+      case Some(winner) => throw new GameOverException(winner)
+      case None => state
