158deac57e51b5b8e4818c8a5acb59f27e411b5e
Gabriele <gabriele.arcese@studio.unibo.it>
2025-07-13 15:16:23 +0200

    added scalaDoc to objectiveCard classes

--- src/core/src/main/scala/engine/ObjectiveValidator.scala
+++ src/core/src/main/scala/engine/ObjectiveValidator.scala
@@ -11,26 +11,45 @@ object ObjectiveValidator:
 
   /**
     * Checks if the given objective is completed by the player in the current game state.
+    *
     * @param objective The objective card to check.
-    * @param gameState The current state of the game.
-    * @param playerState The state of the player.
+    * @param gameState The current state of the game, including board information.
+    * @param playerState The current state of the player.
     * @return True if the objective is completed, false otherwise.
     */
   def done(objective: Option[ObjectiveCard], gameState: GameState, playerState: PlayerState): Boolean =
     objective.exists(card => evaluateObjective(card, gameState, playerState))
     
+  /**
+    * Evaluates whether the given objective is satisfied by the player.
+    *
+    * @param objective The objective to evaluate.
+    * @param gameState The current state of the game.
+    * @param playerState The state of the player being evaluated.
+    * @return True if the objective conditions are met, false otherwise.
+    */
   private def evaluateObjective(objective: ObjectiveCard, gameState: GameState, playerState: PlayerState): Boolean =
     objective match
+      /**
+        * Checks if the player owns at least the specified number of territories,
+        * each with a minimum number of troops.
+        */
       case ObjectiveCard.ConquerTerritories(num, minTroopsToOwn) =>
         val owned = gameState.board.territories.count: territory =>
           territory.owner.exists(_.id == playerState.playerId) && territory.troops >= minTroopsToOwn
         owned >= num
 
+      /**
+        * Checks if the player fully owns all the continents listed in the objective.
+        */
       case ObjectiveCard.ConquerContinents(continents) =>
         continents.forall: continent =>
           gameState.board.continents.find(_.name == continent.name)
             .exists(_.isFullyOwnedBy(playerState.playerId))
 
+      /**
+        * Checks if the player fully owns at least the specified number of any continents.
+        */
       case ObjectiveCard.ConquerNContinents(n) =>
         val ownedContinents = gameState.board.continents.count: continent =>
           continent.isFullyOwnedBy(playerState.playerId)
--- src/core/src/main/scala/model/cards/ObjectiveCard.scala
+++ src/core/src/main/scala/model/cards/ObjectiveCard.scala
@@ -3,20 +3,49 @@ package model.cards
 import model.player.PlayerColor
 import model.board.Continent
 
+/**
+ * Represents a generic objective card in the game.
+ * Each objective card provides a textual description of the mission to be completed by the player.
+ */
 sealed trait ObjectiveCard:
+  /**
+   * A textual description of the objective to display to the player.
+   * 
+   * @return a human-readable description of the objective
+   */
   def description: String
 
+/**
+ * Companion object for the ObjectiveCard trait containing specific types of objective cards.
+ */
 object ObjectiveCard:
 
+  /**
+   * Objective requiring the player to conquer a specific number of territories,
+   * each with a minimum number of troops.
+   *
+   * @param num               the number of territories to conquer
+   * @param minTroopsToOwn    the minimum number of troops each conquered territory must have
+   */
   case class ConquerTerritories(num: Int, minTroopsToOwn: Int = 1) extends ObjectiveCard:
     override def description: String =
       s"Conquer $num territories with at least $minTroopsToOwn troops each."
 
+  /**
+   * Objective requiring the player to conquer a specific set of continents.
+   *
+   * @param continents the set of continents the player must conquer
+   */
   case class ConquerContinents(continents: Set[Continent]) extends ObjectiveCard:
     override def description: String =
       val continentNames = continents.map(_.name).mkString(", ")
       s"Conquer the following continents: $continentNames."
 
+  /**
+   * Objective requiring the player to conquer any N continents.
+   *
+   * @param n the number of continents to conquer (any of them)
+   */
   case class ConquerNContinents(n: Int) extends ObjectiveCard:
     override def description: String =
       s"Conquer any $n continents"
